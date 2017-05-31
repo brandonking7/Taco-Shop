4b0d46fa485df75460b4a8b4f5f6ac62bad75eae
brandonking7 <kingrome74@gmail.com>
2017-05-31 16:54:20 -0400

    Completed Tacos Crud

--- app/controllers/tacos_controller.rb
+++ app/controllers/tacos_controller.rb
@@ -2,17 +2,37 @@ class TacosController < ApplicationController
 
   def index
     @tacos = Taco.all
-  end
 
-  def new
-
+    render json: @tacos
   end
 
   def create
+    @taco = Taco.new(taco_params)
 
+    respond_to do | format |
+      if @taco.save
+        format.html { redirect_to @taco, notice: "Taco was successfully created." }
+        format.json { render json: @taco, status: :created }
+      else
+        format.json { render json: @taco.errors, status: :unprocessable_entity }
+      end
+      end
   end
 
   def destroy
+    @taco = Taco.find(params[:id])
 
+    @taco.destroy
+    respond_to do |format|
+      format.html { redirect_to jobs_url, notice: 'Taco was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+
+  def taco_params
+    params.require(:taco)
+          .permit(:meat, :rice, :salsa, :notes)
   end
 end
